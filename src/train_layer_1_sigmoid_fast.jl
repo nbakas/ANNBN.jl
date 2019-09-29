4b190503d0dfb98e66d38d044d52042c2f199673
nbakas <36529768+nbakas@users.noreply.github.com>
2019-09-29 21:26:05 +0300

    Add files via upload

--- src/train_layer_1_sigmoid_fast.jl
+++ src/train_layer_1_sigmoid_fast.jl
@@ -1,89 +1,28 @@
 function train_layer_1_sigmoid_fast(neurons,vars,obs,n_per_part,inds_all,x,y)
 
-    # a_all=zeros(neurons,vars+1)
     a_all=Vector{Vector{Float64}}()
     ooops=0
-    i_keep=Vector{Int64}()
-    for i=1:neurons # num_ranges^vars
+    for i=1:neurons
         # if i in 1:10:neurons println(Dates.format(now(), "HH:MM:SS")," Calculating weights w for ",i," neuron, of ",neurons," Total.") end
-        # if n_per_part[i+1]-n_per_part[i]!=0
-
-            try
-
-
-                # itmp=randperm(obs)[1:(12+1)]
-                # atmp=[ones(12+1) x[itmp,:]]\isigm1.(y[itmp])
-                # obs1=copy(12+1)
-
-
-                atmp=[ones(n_per_part[i+1]-n_per_part[i]) x[inds_all[n_per_part[i]+1:n_per_part[i+1]],:]]\isigm1.(y[inds_all[n_per_part[i]+1:n_per_part[i+1]]])
-                obs1=length(inds_all[n_per_part[i]+1:n_per_part[i+1]])
-
-
-
-
-                # if sum(isnan.(atmp))==0
-                #     # a_all[i,:].=atmp
-                #     err=[ones(n_per_part[i+1]-n_per_part[i]) x[inds_all[n_per_part[i]+1:n_per_part[i+1]],:]]*atmp.-isigm1.(y[inds_all[n_per_part[i]+1:n_per_part[i+1]]])
-                #     # println(i," ",mean(abs.(err)))
-                # else
-                #     ooops+=1
-                #     # println("local inverse problem, neuron=",i," total ooops=",ooops)
-                # end
-                # # println(a_all[i,:])
-
-
-                # println(i," ",obs1)
-
-                # if sum(isnan.(atmp))==0 && sum(isinf.(atmp))==0 push!(a_all,atmp)
-                # else push!(a_all,zeros(vars+1)); ooops+=1 end
-
-                if sum(isnan.(atmp))==0 && sum(isinf.(atmp))==0 && sum(abs.(atmp[2:end]))>0
-                    push!(a_all,atmp)
-                    push!(i_keep,i)
-                else
-                    ooops+=1
-                end
-
-            catch ex
+        try
+            atmp=[ones(n_per_part[i+1]-n_per_part[i]) x[inds_all[n_per_part[i]+1:n_per_part[i+1]],:]]\isigm1.(y[inds_all[n_per_part[i]+1:n_per_part[i+1]]])
+            obs1=length(inds_all[n_per_part[i]+1:n_per_part[i+1]])
+            if sum(isnan.(atmp))==0 && sum(isinf.(atmp))==0 && sum(abs.(atmp[2:end]))>0
+                push!(a_all,atmp)
+            else
                 ooops+=1
-                println("problem neuron=",i," total ooops=",ooops," ex=",ex)
-                # push!(a_all,zeros(length(inds_all[n_per_part[i]+1:n_per_part[i+1]])))
-                push!(a_all,zeros(vars+1))
-                # continue
             end
-        # end
-        # println("calculating weights for neuron ",i," of ",neurons)
+        catch ex
+            ooops+=1
+            println("problem neuron=",i," total ooops=",ooops," ex=",ex)
+            push!(a_all,zeros(vars+1))
+        end
     end
-    # println("ooops=",ooops)
-    # layer1=zeros(obs,neurons)
-    # for i=1:neurons
-    #     if i in 1:100:neurons println(Dates.format(now(), "HH:MM:SS")," Applying sigmoid on ",i," neuron, of ",neurons," Total.") end
-    #     # layer1[:,i].=sigm1.([ones(obs) x]*a_all[i,:])
-    #     # println(size(a_all[i]))
-    #     layer1[:,i].=sigm1.([ones(obs) x]*a_all[i])
-    #     # x[i_so,:]*a_all[i,:]
-    # end
-    # n_per_part=[n_per_part[1];n_per_part[i_keep.+1]]
-    # a_all=a_all[i_keep]
-
-    # println(Dates.format(now(), "HH:MM:SS")," Constructing Layer 1")
     layer1=sigm1.([ones(obs) x]*reduce(hcat, a_all))
-
-
-    # println("calculating linear terms...")
-    # a_layer1=[sigm1.(layer1) ones(obs)]\y
-    # println(Dates.format(now(), "HH:MM:SS")," Solving system (",obs," by ",size(layer1,2)+1,") for weights v in the output Layer")
-
-    # a_layer1=[layer1 ones(obs)]\y
-    # println(layer1'*layer1)
     mat1=inv([layer1 ones(obs)]'*[layer1 ones(obs)])
+    # println(Dates.format(now(), "HH:MM:SS")," Solving system (",obs," by ",size(layer1,2)+1,") for weights v in the output Layer")
     a_layer1=mat1*[layer1 ones(obs)]'*y
-
     # println(Dates.format(now(), "HH:MM:SS")," All weights computed.")
-    # predl1=[sigm1.(layer1) ones(obs)]*a_layer1
-
-
     return a_all,a_layer1,layer1,mat1
 
 end
--- test/MNIST/__MNIST.jl
+++ test/MNIST/__MNIST.jl
@@ -1,6 +1,10 @@
 
 # With this file you may reproduce the results for cases 1, 2, 3, 4 in Table 2,
-# as well as for Random Forests and Gradient Boosting
+# by changing the number of neurons (variable neurons below), and ϵ (tol1).
+# The activation function and it's inverse may be changed in the main file ANNBN.jl
+# The Random Forests and Gradient Boosting calls utilized in the manuscript are aplso appended bellow
+
+# 1st Step: Read the MNIST Database
 using Plots, Statistics, MLDatasets, LinearAlgebra, Clustering, Printf, Dates, Random, MLDatasets, Plots
 using Printf, Debugger, IterativeSolvers
 train_x, yy_train_all = MLDatasets.MNIST.traindata()
@@ -15,23 +19,22 @@ i_train=size(xx_train,1)
 i_test=size(xx_test,1)
 vars=size(xx_train,2)
 
-tol1=0.02
-num_to_test=0
+# Import ANNBN module
+path1=realpath(dirname(@__FILE__)*"/../..")
+include(string(path1,"/src/ANNBN.jl"))
+
+
+# 2nd Step: Train for a single digit num_to_test. tol1=ϵ normalization in manuscript
+num_to_test=0;tol1=0.02;
 yy_train=copy(yy_train_all)
 i1=abs.(yy_train.-num_to_test).<0.01;i2=abs.(yy_train.-num_to_test).>0.01;yy_train[i1].=1;;yy_train[i2].=0;
 mi=minimum(yy_train);yy_train.-=mi;ma=maximum(yy_train);yy_train./=ma;yy_train.*=1.0-2tol1;yy_train.+=tol1
-
 yy_test=copy(yy_test_all)
 i1=abs.(yy_test.-num_to_test).<0.01;i2=abs.(yy_test.-num_to_test).>0.01;yy_test[i1].=1;yy_test[i2].=0;
 yy_test.-=mi;yy_test./=ma;yy_test.*=1.0-2tol1;yy_test.+=tol1
 
-
-path1=realpath(dirname(@__FILE__)*"/../..")
-include(string(path1,"/src/ANNBN.jl"))
-
-inds_all=1:i_train
-# For higher accuracy, increase the number of neurons. However memory demands increases as well.
-neurons=1000;items_per_neuron=(Int64(floor(i_train/(neurons))))*ones(Int64,neurons);n_per_part=[0;cumsum(items_per_neuron)];n_per_part[end]=i_train
+neurons=1000; # For higher accuracy, increase the number of neurons. RAM demands will increase as well.
+inds_all=1:i_train;items_per_neuron=(Int64(floor(i_train/(neurons))))*ones(Int64,neurons);n_per_part=[0;cumsum(items_per_neuron)];n_per_part[end]=i_train
 @time a_all,a_layer1,layer1,mat1=ANNBN.train_layer_1_sigmoid_fast(neurons,vars,i_train,n_per_part,inds_all,xx_train,yy_train)
 predl1=[layer1 ones(i_train)]*a_layer1
 maetr=mean(abs.(yy_train-predl1))
@@ -42,47 +45,36 @@ maximum(abs.(yy_test-predl1_test))
 i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<1e-10;maetr=100sum(i2)/length(i2)
 i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<1e-10;
 maete=100sum(i2)/length(i2)
-100mean(abs.(round.(predl1).-round.(yy_train)).<0.1)
-100mean(abs.(round.(predl1_test).-round.(yy_test)).<0.1)
+maetr=100mean(abs.(round.(predl1).-round.(yy_train)).<0.1)
+maete=100mean(abs.(round.(predl1_test).-round.(yy_test)).<0.1)
 
 
 
-
+# 3rd Step: Train all digits
 maetrs=Array{Float64}(undef,0)
 maetes=Array{Float64}(undef,0)
 dts=Array{Float64}(undef,0)
 predl1_train_all=Array{Float64}(undef,i_train,0)
 predl1_test_all=Array{Float64}(undef,i_test,0)
+neurons=1000;tol1=0.02;
 @time for num_to_test=0:9
-    global yy_train_all,yy_test_all,predl1_train_all,predl1_test_all
-    yy_train=copy(yy_train_all)
-    yy_test=copy(yy_test_all)
-    tol1=0.02
+    global yy_train_all,yy_test_all,predl1_train_all,predl1_test_all,neurons,tol1
     yy_train=copy(yy_train_all)
     i1=abs.(yy_train.-num_to_test).<0.01;i2=abs.(yy_train.-num_to_test).>0.01;yy_train[i1].=1;;yy_train[i2].=0;
     mi=minimum(yy_train);yy_train.-=mi;ma=maximum(yy_train);yy_train./=ma;yy_train.*=1.0-2tol1;yy_train.+=tol1
     yy_test=copy(yy_test_all)
     i1=abs.(yy_test.-num_to_test).<0.01;i2=abs.(yy_test.-num_to_test).>0.01;yy_test[i1].=1;yy_test[i2].=0;
     yy_test.-=mi;yy_test./=ma;yy_test.*=1.0-2tol1;yy_test.+=tol1
-    inds_all=1:i_train;neurons=7000;items_per_neuron=(Int64(floor(i_train/(neurons))))*ones(Int64,neurons);n_per_part=[0;cumsum(items_per_neuron)];n_per_part[end]=i_train
-    t1=now()
-    @time a_all,a_layer1,layer1=ANNBN.train_layer_1_sigmoid_fast(neurons,vars,i_train,n_per_part,inds_all,xx_train,yy_train)
+    inds_all=1:i_train;items_per_neuron=(Int64(floor(i_train/(neurons))))*ones(Int64,neurons);n_per_part=[0;cumsum(items_per_neuron)];n_per_part[end]=i_train
+    t1=now();a_all,a_layer1,layer1=ANNBN.train_layer_1_sigmoid_fast(neurons,vars,i_train,n_per_part,inds_all,xx_train,yy_train)
     t2=now();dt=convert(Float64,Dates.value(t2-t1))/1000;push!(dts,dt)
-    predl1=[layer1 ones(i_train)]*a_layer1
-    maetr=mean(abs.(yy_train-predl1))
-    maximum(abs.(yy_train-predl1))
-    predl1_test,layer1_test=ANNBN.predict_new(a_all,a_layer1,xx_test,i_test,neurons);
-    maete=mean(abs.(yy_test-predl1_test))
-    maximum(abs.(yy_test-predl1_test))
+    predl1=[layer1 ones(i_train)]*a_layer1; maetr=mean(abs.(yy_train-predl1))
+    predl1_test,layer1_test=ANNBN.predict_new(a_all,a_layer1,xx_test,i_test,neurons); maete=mean(abs.(yy_test-predl1_test))
 
-
-    predl1_train_all=[predl1_train_all predl1]
-    predl1_test_all=[predl1_test_all predl1_test]
-
+    predl1_train_all=[predl1_train_all predl1]; predl1_test_all=[predl1_test_all predl1_test]
     i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<0.01;maetr=100sum(i2)/length(i2)
     i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<0.01;maete=100sum(i2)/length(i2)
-    push!(maetrs,maetr)
-    push!(maetes,maete)
+    push!(maetrs,maetr);push!(maetes,maete)
     println(num_to_test," ",maete)
 end
 println(dts)
@@ -102,30 +94,6 @@ for i=1:i_test
 end
 CC=100-100mean(yy_test_all-yy_test_pred.>0.01)
 
-
-
-using Base.Threads
-
-nthreads()
-x=zeros(10)
-@time begin
-    Threads.@threads for i=1:10
-        for j=1:1e7
-            global x
-            x[i]+=1
-            # println("i = $i on thread $(Threads.threadid())")
-        end
-    end
-end
-@time begin
-    for i=1:10
-        for j=1:1e7
-            global x
-            x[i]+=1
-            # println("i = $i on thread $(Threads.threadid())")
-        end
-    end
-end
 
 
 
@@ -215,157 +183,6 @@ dts=Array{Float64}(undef,0)
 
     i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<0.01;maetr=100sum(i2)/length(i2)
     i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<0.01;maete=100sum(i2)/length(i2)
-    push!(maetrs,maetr)
-    push!(maetes,maete)
-    println(" > > > > > > > > > > > > >",num_to_test," ",maete," ",dt)
-end
-println(dts)
-println(maetes)
-
-
-
-
-
-
-
-# # # # # # # # # # # # # # Flux
-using Flux
-using Statistics
-using Flux: onehotbatch, onecold, crossentropy, throttle
-using Base.Iterators: repeated
-imgs = Flux.Data.MNIST.images()
-labels = Flux.Data.MNIST.labels()
-test_X = hcat(float.(reshape.(Flux.Data.MNIST.images(:test), :))...)
-
-tol1=0.0
-num_to_test=0
-yy_train=copy(labels)
-i1=abs.(yy_train.-num_to_test).<0.01;i2=abs.(yy_train.-num_to_test).>0.01;yy_train[i1].=1;;yy_train[i2].=0;
-mi=minimum(yy_train);yy_train.-=mi;ma=maximum(yy_train);yy_train./=ma;yy_train.*=1.0-2tol1;yy_train.+=tol1
-
-yy_test=Flux.Data.MNIST.labels(:test)
-i1=abs.(yy_test.-num_to_test).<0.01;i2=abs.(yy_test.-num_to_test).>0.01;yy_test[i1].=1;yy_test[i2].=0;
-yy_test.-=mi;yy_test./=ma;yy_test.*=1.0-2tol1;yy_test.+=tol1
-
-
-
-X = hcat(float.(reshape.(imgs, :))...)
-Y = onehotbatch(yy_train,0:1)
-m = Chain(
-  Dense(28^2, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 1000, relu),
-  Dense(1000, 2),
-  softmax)
-loss(x, y) = crossentropy(m(x), y)
-opt = ADAM();
-dataset = repeated((X,Y),30)
-evalcb = () -> @show(loss(X, Y))
-@time for ep=1:3 Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10)); end
-
-predl1=zeros(60000); mmm=m(X); predl1=Tracker.data(mmm[2,:])
-i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<0.01;maetr=100sum(i2)/length(i2)
-predl1_test=zeros(10000); mmm=m(test_X); predl1_test=Tracker.data(mmm[2,:]);
-i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<0.01;
-maete=100sum(i2)/length(i2)
-
-weights = Tracker.data.(params(m))
-
-maetrs=Array{Float64}(undef,0)
-maetes=Array{Float64}(undef,0)
-dts=Array{Float64}(undef,0)
-@time for num_to_test=0:9
-    global imgs,labels,test_X, tol1
-
-    yy_train=copy(labels)
-    i1=abs.(yy_train.-num_to_test).<0.01;i2=abs.(yy_train.-num_to_test).>0.01;yy_train[i1].=1;;yy_train[i2].=0;
-    mi=minimum(yy_train);yy_train.-=mi;ma=maximum(yy_train);yy_train./=ma;yy_train.*=1.0-2tol1;yy_train.+=tol1
-
-    yy_test=Flux.Data.MNIST.labels(:test)
-    i1=abs.(yy_test.-num_to_test).<0.01;i2=abs.(yy_test.-num_to_test).>0.01;yy_test[i1].=1;yy_test[i2].=0;
-    yy_test.-=mi;yy_test./=ma;yy_test.*=1.0-2tol1;yy_test.+=tol1
-
-    X = hcat(float.(reshape.(imgs, :))...)
-    Y = onehotbatch(yy_train,0:1)
-    m = Chain(
-      Dense(28^2, 5000, relu),
-      # Dense(5000, 5000, relu),
-      Dense(5000, 2),
-      softmax)
-    loss(x, y) = crossentropy(m(x), y)
-    opt = ADAM();
-    dataset = repeated((X,Y),30)
-    evalcb = () -> @show(loss(X, Y))
-
-    t1=now()
-    for ep=1:3 Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10)); end
-    t2=now();dt=convert(Float64,Dates.value(t2-t1))/1000;push!(dts,dt)
-
-    predl1=zeros(60000); mmm=m(X); predl1=Tracker.data(mmm[2,:])
-    i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<0.01;maetr=100sum(i2)/length(i2)
-    predl1_test=zeros(10000); mmm=m(test_X); predl1_test=Tracker.data(mmm[2,:]);
-    i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<0.01;
-    maete=100sum(i2)/length(i2)
-
-    push!(maetrs,maetr)
-    push!(maetes,maete)
-    println(" > > > > > > > > > > > > >",num_to_test," ",maete," ",dt)
-end
-println(dts)
-println(maetes)
-
-
-maetrs=Array{Float64}(undef,0)
-maetes=Array{Float64}(undef,0)
-dts=Array{Float64}(undef,0)
-@time for num_to_test=0:9
-    global imgs,labels,test_X, tol1
-
-    yy_train=copy(labels)
-    i1=abs.(yy_train.-num_to_test).<0.01;i2=abs.(yy_train.-num_to_test).>0.01;yy_train[i1].=1;;yy_train[i2].=0;
-    mi=minimum(yy_train);yy_train.-=mi;ma=maximum(yy_train);yy_train./=ma;yy_train.*=1.0-2tol1;yy_train.+=tol1
-
-    yy_test=Flux.Data.MNIST.labels(:test)
-    i1=abs.(yy_test.-num_to_test).<0.01;i2=abs.(yy_test.-num_to_test).>0.01;yy_test[i1].=1;yy_test[i2].=0;
-    yy_test.-=mi;yy_test./=ma;yy_test.*=1.0-2tol1;yy_test.+=tol1
-
-    X = hcat(float.(reshape.(imgs, :))...)
-    Y = onehotbatch(yy_train,0:1)
-    m = Chain(
-    Dense(28^2, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 1000, relu),
-    Dense(1000, 2),
-      softmax)
-    loss(x, y) = crossentropy(m(x), y)
-    opt = ADAM();
-    dataset = repeated((X,Y),50)
-    evalcb = () -> @show(loss(X, Y))
-
-    t1=now()
-    for ep=1:3 Flux.train!(loss, params(m), dataset, opt, cb = throttle(evalcb, 10)); end
-    t2=now();dt=convert(Float64,Dates.value(t2-t1))/1000;push!(dts,dt)
-
-    predl1=zeros(60000); mmm=m(X); predl1=Tracker.data(mmm[2,:])
-    i1=predl1.<=0.5;predl1[i1].=tol1;i1=predl1.>0.5;predl1[i1].=1.0-tol1;i2=abs.(predl1.-yy_train).<0.01;maetr=100sum(i2)/length(i2)
-    predl1_test=zeros(10000); mmm=m(test_X); predl1_test=Tracker.data(mmm[2,:]);
-    i1=predl1_test.<=0.5;predl1_test[i1].=tol1;i1=predl1_test.>0.5;predl1_test[i1].=1.0-tol1;i2=abs.(predl1_test.-yy_test).<0.01;
-    maete=100sum(i2)/length(i2)
-
     push!(maetrs,maetr)
     push!(maetes,maete)
     println(" > > > > > > > > > > > > >",num_to_test," ",maete," ",dt)
